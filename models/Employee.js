15ff15c7e21f32bcdb137534577977ab007f65e7
Adrian Hansa <adrian.hansa@yahoo.com>
2022-01-06 19:15:33 +0000

    Created Employee model

---
+++ models/Employee.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    inProbation: { type: Boolean, default: false },
+    payrollNumber: { type: String, unique: true, default: 0 },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "companies",
+      required: true,
+    },
+    service: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "services",
+      required: true,
+    },
+    address: { type: String, default: "" },
+    phone: { type: String, default: "" },
+    driver: { type: Boolean, default: false },
+    contractHours: { type: Number, default: 0 },
+    role: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("employees", employeeSchema);
