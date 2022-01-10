58fe058aa9330bd74dd8c39be60c0e0895ad718b
Adrian Hansa <adrian.hansa@yahoo.com>
2022-01-10 07:12:27 +0000

    Created getShift, updateShift and deleteShift endpoints.

--- controllers/workShiftController.js
+++ controllers/workShiftController.js
@@ -31,7 +31,33 @@ const addWorkShift = async (req, res) => {
 
 const updateWorkShift = async (req, res) => {
   try {
-    //
+    const { date, employee, shift, startTime, endTime, notes, workingStatus } =
+      req.body;
+    if (
+      !date ||
+      !employee ||
+      !shift ||
+      !startTime ||
+      !endTime ||
+      !workingStatus
+    )
+      return res.status(400).json({ message: "All fields are required." });
+    const workShift = await WorkShift.findByIdAndUpdate(
+      req.params.id,
+      {
+        date,
+        employee,
+        shift,
+        startTime,
+        endTime,
+        notes,
+        workingStatus,
+      },
+      { new: true }
+    );
+    if (!workShift)
+      return res.status(404).json({ message: "Working shift not found" });
+    res.status(200).json(workShift);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -39,7 +65,10 @@ const updateWorkShift = async (req, res) => {
 
 const deleteWorkShift = async (req, res) => {
   try {
-    //
+    const workShift = await WorkShift.findByIdAndDelete(req.params.id);
+    if (!workShift)
+      return res.status(404).json({ message: "Working shift not found" });
+    res.status(200).json({ message: "The shift could not be found." });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -47,7 +76,10 @@ const deleteWorkShift = async (req, res) => {
 
 const getWorkShift = async (req, res) => {
   try {
-    //
+    const workShift = await WorkShift.findById(req.params.id);
+    if (!workShift)
+      return res.status(404).json({ message: "Working shift not found" });
+    res.status(200).json(workShift);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
