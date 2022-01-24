a539dffa890a6b09d29f1d068f12830a989fbab0
Adrian Hansa <adrian.hansa@yahoo.com>
2022-01-24 18:59:29 +0000

    Completed all handover endpoints method in the controller

--- controllers/handoverController.js
+++ controllers/handoverController.js
@@ -2,7 +2,19 @@ const Handover = require("../models/Handover");
 
 const addHandoverEntry = async (req, res) => {
   try {
-    //
+    const { date, time, notes } = req.body;
+    if (!date || !time || !notes)
+      return res
+        .status(400)
+        .json({ message: "Please fill in all the fields." });
+    const handoverEntry = await Handover.create({
+      date,
+      time,
+      notes,
+      service: req.service,
+      employee: req.user.id,
+    });
+    res.status(200).json(handoverEntry);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -10,7 +22,19 @@ const addHandoverEntry = async (req, res) => {
 
 const updateHandoverEntry = async (req, res) => {
   try {
-    //
+    const { date, time, notes } = req.body;
+    if (!date || !time || !notes)
+      return res
+        .status(400)
+        .json({ message: "Please fill in all the fields." });
+    const handoverEntry = await Handover.findByIdAndUpdate(
+      req.params.id,
+      { date, time, notes, employee: req.user.id },
+      { new: true }
+    );
+    if (!handoverEntry)
+      return res.status(404).json({ message: "Handover entry not found." });
+    res.status(200).json(handoverEntry);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -18,7 +42,10 @@ const updateHandoverEntry = async (req, res) => {
 
 const getHandoverEntry = async (req, res) => {
   try {
-    //
+    const handoverEntry = await Handover.findById(req.params.id);
+    if (!handoverEntry)
+      return res.status(404).json({ message: "Handover entry not found" });
+    res.status(200).json(handoverEntry);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -26,7 +53,8 @@ const getHandoverEntry = async (req, res) => {
 
 const getHandoverEntries = async (req, res) => {
   try {
-    //
+    const handoverEntries = await Handover.find({ service: req.service });
+    res.status(200).json(handoverEntries);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -34,7 +62,10 @@ const getHandoverEntries = async (req, res) => {
 
 const deleteHandoverEntry = async (req, res) => {
   try {
-    //
+    const handoverEntry = await Handover.findByIdAndDelete(req.params.id);
+    if (!handoverEntry)
+      return res.status(404).json({ message: "Handover entry not found" });
+    res.status(200).json({ message: "Handover deleted successfully." });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
